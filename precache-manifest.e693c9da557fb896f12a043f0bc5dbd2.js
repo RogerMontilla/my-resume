daf1c7a4d0f82da93378be9db55f046484ee53a2
Roger_Montilla <rogermontilla01@gmail.com>
2020-10-08 21:29:10 -0300

    Updates

---
+++ precache-manifest.e693c9da557fb896f12a043f0bc5dbd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f3211dca10f8489ed5790d22448bd5",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "4734012852ebd370dd4d",
+    "url": "/my-resume/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ab00afd3eba8cf19794a",
+    "url": "/my-resume/static/css/main.c6cfeb0a.chunk.css"
+  },
+  {
+    "revision": "4734012852ebd370dd4d",
+    "url": "/my-resume/static/js/2.8234342d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-resume/static/js/2.8234342d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab00afd3eba8cf19794a",
+    "url": "/my-resume/static/js/main.5d608895.chunk.js"
+  },
+  {
+    "revision": "a6efcd828d3624d74f01",
+    "url": "/my-resume/static/js/runtime-main.fb0541e1.js"
+  }
+]);
